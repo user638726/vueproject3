abc598405921efc45c30aecc8206626041d3a5c2
user638726 <lienjackson35@gmail.com>
2026-01-19 01:06:24 +0800

    建構網站Part12

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,21 @@ export default {
   components: {
     TheHeader,
   },
+  computed: {
+    didAutoLogout() {
+      return this.$store.getters.didAutoLogout;
+    },
+  },
+  created() {
+    this.$store.dispatch('tryLogin');
+  },
+  watch: {
+    didAutoLogout(curValue, oldValue) {
+      if (curValue && curValue !== oldValue) {
+        this.$router.replace('/coaches');
+      }
+    },
+  },
 };
 </script>
 
@@ -32,27 +47,26 @@ body {
   margin: 0;
 }
 
-.route-enter-from{
-  opacity:0;
-  transform:translateY(-30px);
+.route-enter-from {
+  opacity: 0;
+  transform: translateY(-30px);
 }
 
-.route-leave-to{
-  opacity:0;
-  transform:translateY(30px);
+.route-leave-to {
+  opacity: 0;
+  transform: translateY(30px);
 }
 
-.route-enter-active{
-   transition: all 0.3s ease-out
+.route-enter-active {
+  transition: all 0.3s ease-out;
 }
 
-.route-leave-active{
-   transition: all 0.3s ease-in
+.route-leave-active {
+  transition: all 0.3s ease-in;
 }
 
-.route-enter-to
-.route-leave-from{
-  opacity:1;
-  transform:translateY(0);
+.route-enter-to .route-leave-from {
+  opacity: 1;
+  transform: translateY(0);
 }
 </style>
--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,23 +1,46 @@
 import axios from 'axios';
 
+let timer;
+
 export default {
   async login(context, payload) {
+    return context.dispatch('auth', {
+      ...payload,
+      mode: 'login',
+    });
+  },
+  async signup(context, payload) {
+    return context.dispatch('auth', {
+      ...payload,
+      mode: 'signup',
+    });
+  },
+  async auth(context, payload) {
+    const { mode } = payload;
+    let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCSooMpbBO_PrzSD_nddN1ageHe8QUVmZg';
+    if (mode === 'signup') {
+      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCSooMpbBO_PrzSD_nddN1ageHe8QUVmZg';
+    }
     try {
-      const response = await axios.post(
-        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCSooMpbBO_PrzSD_nddN1ageHe8QUVmZg',
-        {
-          email: payload.email,
-          password: payload.password,
-          returnSecureToken: true,
-        },
-      );
+      const response = await axios.post(url, {
+        email: payload.email,
+        password: payload.password,
+        returnSecureToken: true,
+      });
       const responseData = response.data;
+      const expiresIn = +responseData.expiresIn * 1000;
+      // const expiresIn = 5000;
+      const expirationDate = new Date().getTime() + expiresIn;
       // TODO: Commit mutation with user data if needed
-      console.log(responseData);
+      localStorage.setItem('token', responseData.idToken);
+      localStorage.setItem('userId', responseData.localId);
+      localStorage.setItem('tokenExpiration', expirationDate);
+      timer = setTimeout(() => {
+        context.dispatch('autoLogout');
+      }, expiresIn);
       context.commit('setUser', {
         token: responseData.idToken,
         userId: responseData.localId,
-        tokenExpiration: responseData.expiresIn,
       });
     } catch (error) {
       // eslint-disable-next-line no-console
@@ -25,36 +48,39 @@ export default {
       throw error;
     }
   },
-  async signup(context, payload) {
-    try {
-      const response = await axios.post(
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCSooMpbBO_PrzSD_nddN1ageHe8QUVmZg',
-        {
-          email: payload.email,
-          password: payload.password,
-          returnSecureToken: true,
-        },
-      );
-      const responseData = response.data;
-      // TODO: Commit mutation with user data if needed
-      // context.commit('setUser', responseData);
-      console.log(responseData);
+  tryLogin(context) {
+    const token = localStorage.getItem('token');
+    const userId = localStorage.getItem('userId');
+    const tokenExpiration = localStorage.getItem('tokenExpiration');
+
+    const expiresIn = +tokenExpiration - new Date().getTime();
+
+    if (expiresIn < 0) {
+      return;
+    }
+    timer = setTimeout(() => {
+      context.dispatch('autoLogout');
+    }, expiresIn);
+
+    if (token && userId) {
       context.commit('setUser', {
-        token: responseData.idToken,
-        userId: responseData.localId,
-        tokenExpiration: responseData.expiresIn,
+        token,
+        userId,
       });
-    } catch (error) {
-      // eslint-disable-next-line no-console
-      console.error('註冊時發生錯誤:', error);
-      throw error;
     }
   },
   logout(context) {
+    localStorage.removeItem('token');
+    localStorage.removeItem('userId');
+    localStorage.removeItem('tokenExpiration');
+    clearTimeout(timer);
     context.commit('setUser', {
       token: null,
       userId: null,
-      tokenExpiration: null,
     });
+  },
+  autoLogout(context) {
+    context.dispatch('logout');
+    context.commit('setAutoLogout');
   },
 };
