558e98b950c4b62f68e89db00967f315d70128e7
user638726 <lienjackson35@gmail.com>
2026-01-19 01:43:00 +0800

    增加Async  component

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,18 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
-import CoachDetail from '@/pages/coaches/CoachDetail.vue';
+// import CoachDetail from '@/pages/coaches/CoachDetail.vue';
 import CoachesList from '@/pages/coaches/CoachesList.vue';
-import CoachRegistration from '@/pages/coaches/CoachRegistration.vue';
-import ContactCoach from '@/pages/requests/ContactCoach.vue';
-import RequestsReceived from '@/pages/requests/RequestsReceived.vue';
+// import CoachRegistration from '@/pages/coaches/CoachRegistration.vue';
+// import ContactCoach from '@/pages/requests/ContactCoach.vue';
+// import RequestsReceived from '@/pages/requests/RequestsReceived.vue';
 import NotFound from '@/pages/NotFound.vue';
-import UserAuth from '@/pages/auth/UserAuth.vue';
+// import UserAuth from '@/pages/auth/UserAuth.vue';
 import store from '@/store/index';
 
+const CoachDetail = () => import('@/pages/coaches/CoachDetail.vue');
+const CoachRegistration = () => import('@/pages/coaches/CoachRegistration.vue');
+const ContactCoach = () => import('@/pages/requests/ContactCoach.vue');
+const RequestsReceived = () => import('@/pages/requests/RequestsReceived.vue');
+const UserAuth = () => import('@/pages/auth/UserAuth.vue');
 const routes = [
   {
     path: '/',
